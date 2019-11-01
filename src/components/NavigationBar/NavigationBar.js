6219ee1a3e07d08900bebaa3babe06979d80f295
Vaibhav Vaingankar <vaibhav_vaingankar@persistent.co.in>
2019-11-01 18:17:49 +0530

    added routing to navigation

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,23 +1,191 @@
 import React from 'react';
 
 import NavBarItem from './NavBarItem/NavBarItem';
+import classes from './NavigationBar.module.css';
 
 const navigationBar = (props) => {
 
     let navItems = [
-        { id: '1', name: 'Home', path: '/' },
-        { id: '2', name: 'Reports', path: '/reports' },
-        { id: '4', name: 'Projects', path: '/projects' },
-        { id: '3', name: 'Baseline Planning', path: '/planning' },
-        { id: '5', name: 'Metrics Baseline Contents', path: '/baseline' },
-        { id: '6', name: 'Project Metrics', path: '/projectMetrics' },
-        { id: '7', name: 'Setting', path: '/settings' }
+        {
+            id: '1',
+            name: 'Home',
+            path: '/',
+            submenu: [
+                {
+                    id: '11',
+                    name: 'Home',
+                    path: '/'
+                },
+                {
+                    id: '12',
+                    name: 'Feedback',
+                    path: '/feedback'
+                },
+                {
+                    id: '13',
+                    name: 'Help',
+                    path: '/help'
+                },
+                {
+                    id: '14',
+                    name: 'Demo Version',
+                    path: '/demoVersion'
+                },
+                {
+                    id: '15',
+                    name: 'User Guide',
+                    path: '/userGuide'
+                },
+                {
+                    id: '16',
+                    name: 'SQA Guide',
+                    path: '/sqaGuide'
+                },
+                {
+                    id: '17',
+                    name: 'Metrics Plan Template',
+                    path: '/metricsPlanTemplate'
+                },
+                {
+                    id: '18',
+                    name: 'Logout',
+                    path: '/logout'
+                }
+            ]
+        },
+        {
+            id: '2',
+            name: 'Reports',
+            path: '/reports',
+            submenu: [
+                {
+                    id: '21',
+                    name: 'Baseline Reports',
+                    path: '/reports/baselineReports'
+                },
+                {
+                    id: '22',
+                    name: 'Dashboard Reports',
+                    path: '/reports/dashboardReports'
+                },
+                {
+                    id: '23',
+                    name: 'Metrics Non Compliance Report',
+                    path: '/reports/metricsNonComplianceReport'
+                },
+                {
+                    id: '24',
+                    name: 'Goal Met Report',
+                    path: '/reports/goalMetReport'
+                },
+                {
+                    id: '25',
+                    name: 'DIR Report',
+                    path: '/reports/dirReport'
+                },
+                {
+                    id: '26',
+                    name: 'Incorrect Master',
+                    path: '/reports/incorrectMaster'
+                },
+                {
+                    id: '27',
+                    name: 'Summary Report',
+                    path: '/reports/summaryReport'
+                }
+            ]
+        },
+        {
+            id: '3',
+            name: 'Projects',
+            path: '/projects',
+            submenu: [
+                {
+                    id: '31',
+                    name: 'Create/Edit Project',
+                    path: '/projects/createEditProject'
+                },
+                {
+                    id: '32',
+                    name: 'Search Project',
+                    path: '/projects/searchProject'
+                },
+                {
+                    id: '33',
+                    name: 'Metrics Plan',
+                    path: '/projects/metricsPlan'
+                }
+            ]
+        },
+        {
+            id: '4',
+            name: 'Baseline Planning',
+            path: '/baselinePlanning',
+            submenu: [
+                {
+                    id: '41',
+                    name: 'Create Baseline Plan',
+                    path: '/baselinePlanning/createBaselinePlan'
+                }
+            ]
+        },
+        {
+            id: '5',
+            name: 'Metrics Baseline Contents',
+            path: '/metricsBaselineContents',
+            submenu: [
+                {
+                    id: '51',
+                    name: 'Add/Remove Project(s)',
+                    path: '/metricsBaselineContents/addRemoveProjects'
+                }
+            ]
+        },
+        {
+            id: '6',
+            name: 'Project Metrics',
+            path: '/projectMetrics',
+            submenu: [
+                {
+                    id: '61',
+                    name: 'Upload Metrics Data',
+                    path: '/projectMetrics/uploadMetricsData'
+                },
+                {
+                    id: '62',
+                    name: 'Metrics Data Validation',
+                    path: '/projectMetrics/metricsDataValidation'
+                }
+            ]
+        },
+        {
+            id: '7',
+            name: 'Setting',
+            path: '/settings',
+            submenu: [
+                {
+                    id: '71',
+                    name: 'User Management',
+                    path: '/settings/userManagement'
+                },
+                {
+                    id: '72',
+                    name: 'Project Mapping',
+                    path: '/settings/projectMapping'
+                },
+                {
+                    id: '73',
+                    name: 'Options',
+                    path: '/settings/options'
+                }
+            ]
+        }
     ];
     return (
-        <div>
+        <div className={classes.NavBar}>
             <ul>
                 {navItems.map(navItem => (
-                    <NavBarItem key={navItem.id} value={navItem.name} path={navItem.path} />
+                    <NavBarItem key={navItem.id} value={navItem.name} path={navItem.path} submenu={navItem.submenu} />
                 ))}
             </ul>
         </div>
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import NavBar from '../../components/NavigationBar/NavigationBar';
-import Home from '../../components/Home/Home';
-import Reports from '../../components/Reports/Reports';
-import Projects from '../../components/Projects/Projects';
-import Planning from '../../components/Planning/Planning';
-import Baseline from '../../components/Baseline/Baseline';
-import ProjectMetrics from '../../components/ProjectMetrics/ProjectMetrics';
-import Settings from '../../components/Settings/Settings';
+import Home from '../../components/Content/Home/Home';
+import Demoversion from '../../components/Content/Home/DemoVersion/Demoversion';
+import Feedback from '../../components/Content/Home/Feedback/Feedback';
+import Help from '../../components/Content/Home/Help/Help';
+import MetricsPlanTemplate from '../../components/Content/Home/MetricsPlanTemplate/MetricsPlanTemplate';
+import SQAGuide from '../../components/Content/Home/SQAGuide/SqaGuide';
+import UserGuide from '../../components/Content/Home/UserGuide/UserGuide';
+import BaselineReports from '../../components/Content/Reports/BaselineReports/BaselineReports';
+import DashboardReports from '../../components/Content/Reports/DashboardReports/DashboardReports';
+import DIRReports from '../../components/Content/Reports/DIRReports/DIRReports';
+import GoalMetReports from '../../components/Content/Reports/GoalMetReports/GoalMetReports';
+import IncorrectMaster from '../../components/Content/Reports/IncorrectMaster/IncorrectMaster';
+import MetricsNonComplianceReports from '../../components/Content/Reports/MetricsNonComplianceReports/MetricsNonComplianceReports';
+import SummaryReport from '../../components/Content/Reports/SummaryReport/SummaryReport';
+import CreateEditProject from '../../components/Content/Projects/CreateEditProject/CreateEditProject';
+import MetricsPlan from '../../components/Content/Projects/MetricsPlan/MetricsPlan';
+import SearchProject from '../../components/Content/Projects/SearchProject/SearchProject';
+import CreateBaselinePlan from '../../components/Content/BaselinePlanning/CreateBaselinePlan/CreateBaselinePlan';
+import AddRemoveProjects from '../../components/Content/MetricsBaselineContent/AddRemoveProject/AddRemoveProject';
+import MetricsDataValidation from '../../components/Content/ProjectMetrics/MetricsDataValidation/MetricsDataValidation';
+import UploadMetricsData from '../../components/Content/ProjectMetrics/UploadMetricsData/UploadMetricsData';
+import Options from '../../components/Content/Settings/Options/Options';
+import ProjectMapping from '../../components/Content/Settings/ProjectMapping/ProjectMapping';
+import UserManagement from '../../components/Content/Settings/UserManagement/UserManagement';
+
+
 
 class Layout extends Component {
     state = {
@@ -21,27 +40,31 @@ class Layout extends Component {
                 <Router>
                     <NavBar />
                     <Switch>
-                        <Route path="/reports">
-                            <Reports />
-                        </Route>
-                        <Route path="/projects">
-                            <Projects />
-                        </Route>
-                        <Route path="/planning">
-                            <Planning />
-                        </Route>
-                        <Route path="/baseline">
-                            <Baseline />
-                        </Route>
-                        <Route path="/projectMetrics">
-                            <ProjectMetrics />
-                        </Route>
-                        <Route path="/settings">
-                            <Settings />
-                        </Route>
-                        <Route path="/">
-                            <Home />
-                        </Route>
+                        <Route path='/reports/baselineReports'><BaselineReports /></Route>
+                        <Route path='/reports/dashboardReports'><DashboardReports /></Route>
+                        <Route path='/reports/metricsNonComplianceReport'><MetricsNonComplianceReports /></Route>
+                        <Route path='/reports/goalMetReport'><GoalMetReports /></Route>
+                        <Route path='/reports/dirReport'><DIRReports /></Route>
+                        <Route path='/reports/incorrectMaster'><IncorrectMaster /></Route>
+                        <Route path='/reports/summaryReport'><SummaryReport /></Route>
+                        <Route path='/projects/createEditProject'><CreateEditProject /></Route>
+                        <Route path='/projects/searchProject'><SearchProject /></Route>
+                        <Route path='/projects/metricsPlan'><MetricsPlan /></Route>
+                        <Route path='/baselinePlanning/createBaselinePlan'><CreateBaselinePlan /></Route>
+                        <Route path='/metricsBaselineContents/addRemoveProjects'><AddRemoveProjects /></Route>
+                        <Route path='/projectMetrics/uploadMetricsData'><UploadMetricsData /></Route>
+                        <Route path='/projectMetrics/metricsDataValidation'><MetricsDataValidation /></Route>
+                        <Route path='/settings/userManagement'><UserManagement /></Route>
+                        <Route path='/settings/projectMapping'><ProjectMapping /></Route>
+                        <Route path='/settings/options'><Options /></Route>
+                        <Route path='/feedback'><Feedback /></Route>
+                        <Route path='/help'><Help /></Route>
+                        <Route path='/demoVersion'><Demoversion /></Route>
+                        <Route path='/userGuide'><UserGuide /></Route>
+                        <Route path='/sqaGuide'><SQAGuide /></Route>
+                        <Route path='/metricsPlanTemplate'><MetricsPlanTemplate /></Route>
+                        <Route path='/logout'><Home /></Route>
+                        <Route path='/'><Home /></Route>
                     </Switch>
                 </Router>
             </div>
